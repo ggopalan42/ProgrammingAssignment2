4280abcc5c3d60309b5862c71df7a21412b32cf4
Giri Gopalan <ggopalan@yahoo.com>
2014-06-20 06:23:37 -0700

    Submitting programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,59 @@
-## Put comments here that give an overall description of what your
-## functions do
+# These functions provide for computing and caching the inverse
+# of a matrix. Since computation of an inverse is often expensive,
+# the computed inverse is stored in a cache and is returned provided the 
+# original matrix has not changed.
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+# This function provides for a set of sub-function that can set, get 
+# compute the inverse of a matrix. This function returns a list of
+# functions that can then be used with the cacheSolve function (below)
+# to access the matrix and its inverse
+makeCacheMatrix <- function(mat = matrix()) {
+    # Initialize the variable mat_inv to null such that the functions
+    # defined below can access them (i.e. this is the parent frame to
+    # the functions defined below)
+    mat_inv <- NULL
 
+    # Given an input matrix, set it. Also "clear the cache" 
+    # (mat_inv <<- NULL) since the original matrix has likely changed
+    set <- function(input_mat)
+    {
+        mat <<- input_mat
+        mat_inv <<- NULL
+    }
+
+    # The get function simply returns the matrix
+    get <- function() mat
+
+    # The setinv function assigns the provided matrix inverse to 
+    # the local mat_inv (this also caches the matrix inverse)
+    setinv <- function(minv) mat_inv <<- minv
+
+    # The getinv function simply returns the (cached) matrix inverse
+    getinv <- function() mat_inv
+
+    # Return a function list of the cache matrix manipulation functions
+    list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+# This function is the access function to the cached matrix inverse
+# When provided with the function list from the makeCachematrix function,
+# it will attempt to access the cached inverse. If the cached inverse is
+# not available, it will compute the inverse, cache it and return the inverse
+cacheSolve <- function(func_list, ...) {
+    # Get the cached inverse
+    mat_inv <- func_list$getinv()
+    # If inverse has been cached from before, return the cached version
+    if(!is.null(mat_inv))
+    {
+        message("Returning cached matrix inverse")
+        return(mat_inv)
+    }
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    # If not, get the matrix, compute inverse, cache it and return the inverse
+    mat <- func_list$get()
+    mat_inv <- solve(mat, ...)
+    func_list$setinv(mat_inv)
+    mat_inv
 }
